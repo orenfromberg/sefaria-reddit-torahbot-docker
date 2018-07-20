132a440f6da87b7cea97396f80fbd472266aa9e0
Lev Eliezer Israel <eliezer.israel@gmail.com>
2018-07-20 11:03:02 -0400

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -3,6 +3,8 @@
 import os.path
 relative_to_abs_path = lambda *x: os.path.join(os.path.dirname(
                                os.path.realpath(__file__)), *x)
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 OFFLINE = False
