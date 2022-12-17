1e69a4bdd732664097da05b08eb9cfd9e67df3c1
Oren Fromberg <orenfromberg@gmail.com>
2022-12-17 01:31:37 -0500

    Moving scripts

--- parashat.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker exec torahbot bash -c "python torahbot-parashat-hashavua.py"
---
+++ scripts/export-titles.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# this script will print a json file of all Sefaria titles for copying to the website.
+docker exec torahbot bash -c "python torahbot-parashat-hashavua.py"
