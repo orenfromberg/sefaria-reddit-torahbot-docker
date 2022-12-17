59431624b229dfb75f6d824b60311ebd530d6329
Oren Fromberg <orenfromberg@gmail.com>
2022-12-17 01:48:37 -0500

    Fixing script

--- scripts/export-titles.sh
+++ scripts/export-titles.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # this script will print a json file of all Sefaria titles for copying to the website.
-docker exec torahbot bash -c "python torahbot-parashat-hashavua.py"
+docker exec torahbot bash -c "python torahbot/scripts/get-titles.py"
